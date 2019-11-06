0dbc7bbae11d002dc6131bef5c012ab199617ed7
心叶 <yelloxing@gmail.com>
2019-11-06 09:27:15 +0800

    文件的删除，移动和复制操作

--- README.md
+++ README.md
@@ -8,6 +8,71 @@
 ## Issues
 使用的时候遇到任何问题或有好的建议，请点击进入[issue](https://github.com/yelloxing/cuf/issues)！
 
+## How to use?
+首先，这是一个辅助你开发node.js脚本的东西（比如对webpack或rollup等打包的补充），你有两种方式可以使用：
+
+- 第一种：通过命令行或package.json命令的方式；
+- 第二种：使用cuf.config.js配置文件方式（可以自定义插件或第三方插件，开发中）。
+
+我们内置了一些方法来辅助你编辑自定义插件或配置文件，首先需要获得cuf（配置文件中可能已经帮你注入了）：
+
+```js
+const cuf = require('cuf');
+```
+
+### 解析命令行参数
+
+- 解析命令行传递的参数，转换为更任意处理的结构： 
+
+```js
+// 其中shortHands是缩写到全拼的映射
+// 例子：{'-c':'--copy','-d':'--delete'}
+const parsed = cuf.option(shortHands, process.argv);
+```
+
+### 文件操作相关
+
+- 移动文件或文件夹
+
+```js
+cuf.moveSync(source, target);
+```
+
+- 删除文件或文件夹
+
+```js
+cuf.deleteSync(target);
+```
+
+- 复制文件或文件夹
+
+```js
+cuf.copySync(source, target);
+```
+
+### 日志打印
+
+- 打印一些重要提示信息
+
+```js
+cuf.log(txt);
+```
+
+- 打印最普通的提示
+
+```js
+cuf.print(txt);
+```
+
+### 路径
+
+- 获取文件或文件夹的全路径
+
+```js
+// contextPath表示路径上下文，可选，默认当前命令行路径
+cuf.fullPath(pathString[, contextPath]);
+```
+
 ## License
 
 [MIT](https://github.com/yelloxing/cuf/blob/master/LICENSE)
--- scripts/file.js
+++ scripts/file.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 /**
  * 
  * 移动文件或文件夹
@@ -7,8 +10,29 @@
  * 
  */
 function moveSync(source, target) {
-  console.log('move');
-  console.log(source, target);
+
+  // 如果是文件，直接剪切即可
+  if (!fs.lstatSync(source).isDirectory()) {
+    fs.copyFileSync(source, target);
+    fs.unlinkSync(source);
+  } else {
+
+    // 读取子文件
+    const subFiles = fs.readdirSync(source);
+
+    // 如果文件夹不存在，创建
+    if (!fs.existsSync(target)) {
+      fs.mkdirSync(target);
+    }
+
+    // 移动子文件或文件夹
+    subFiles.forEach(function (file) {
+      moveSync(path.join(source, "./" + file), path.join(target, "./" + file));
+    });
+
+    // 移动完子文件或文件夹以后（移动完毕也意味着子文件或文件夹被删除了）
+    fs.rmdirSync(source);
+  }
 };
 
 /**
@@ -20,8 +44,26 @@ function moveSync(source, target) {
  * 
  */
 function copySync(source, target) {
-  console.log('copy');
-  console.log(source, target);
+
+  // 如果是文件，直接复制即可
+  if (!fs.lstatSync(source).isDirectory()) {
+    fs.copyFileSync(source, target);
+  } else {
+
+    // 读取子文件
+    const subFiles = fs.readdirSync(source);
+
+    // 如果文件夹不存在，创建
+    if (!fs.existsSync(target)) {
+      fs.mkdirSync(target);
+    }
+
+    // 复制子文件或文件夹
+    subFiles.forEach(function (file) {
+      copySync(path.join(source, "./" + file), path.join(target, "./" + file));
+    });
+
+  }
 };
 
 /**
@@ -32,8 +74,26 @@ function copySync(source, target) {
  * 
  */
 function deleteSync(target) {
-  console.log('delete');
-  console.log(target);
+
+  // 如果是文件，直接删除即可
+  if (!fs.lstatSync(target).isDirectory()) {
+    fs.unlinkSync(target);
+  } else {
+
+    // 读取子文件
+    const subFiles = fs.readdirSync(target);
+
+    subFiles.forEach(function (file) {
+
+      // 调用这个方法，删除子文件或文件夹
+      const curPath = path.join(target, "./" + file);
+      deleteSync(curPath);
+
+    });
+
+    // 等子文件或文件夹删除完毕以后，删除本文件夹
+    fs.rmdirSync(target);
+  }
 };
 
 // 导出
