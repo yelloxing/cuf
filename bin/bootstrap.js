4a7d59843c825bebe50c362d99af6b5da8943c33
心叶 <yelloxing@gmail.com>
2019-11-08 15:04:49 +0800

    优化cuf.config.js

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -1,7 +1,9 @@
 const cuf = require('../index');
 const fs = require('fs');
+const __ = require('@yelloxing/core.js');
 
 module.exports = function (configUrl, taskName) {
+  taskName = taskName || "default";
 
   // 如果配置文件存在，且是.js文件
   if (/\.js$/.test(configUrl) && fs.existsSync(configUrl) && !fs.lstatSync(configUrl).isDirectory()) {
@@ -11,8 +13,12 @@ module.exports = function (configUrl, taskName) {
     // 获取package.json文件
     let packageJSON = JSON.parse(fs.readFileSync(cuf.fullPath(configFile.pkg + '/package.json', configFile.path)));
 
+    if (__.isFunction(configFile.task)) {
+      configFile.task['default'] = configFile.task;
+    }
+
     // 执行任务
-    configFile.task[taskName](cuf, packageJSON);
+    configFile.task[taskName](cuf, packageJSON, configFile.path);
 
   } else {
     cuf.error('No config file:' + configUrl);
--- test/demo/cuf.config.js
+++ test/demo/cuf.config.js
@@ -10,22 +10,47 @@ module.exports = {
   task: {
 
     // 任务一
-    demo1: function (cuf, pkg) {
+    // 执行命令：cuf --config ./cuf.config.js demo1
+    demo1: function (cuf, pkg, path) {
 
       cuf.log('执行任务一');
       cuf.print(cuf);
       cuf.print(pkg);
+      cuf.print(path);
 
     },
 
     // 任务二
-    demo2: function (cuf, pkg) {
+    // 执行命令：cuf --config ./cuf.config.js demo2
+    demo2: function (cuf, pkg, path) {
 
       cuf.log('执行任务二');
       cuf.print(cuf);
       cuf.print(pkg);
+      cuf.print(path);
 
-    }
+    },
+
+    // 默认任务
+    // 执行命令：cuf --config ./cuf.config.js
+    default: function (cuf, pkg, path) {
+
+      cuf.log('执行默认任务');
+      cuf.print(cuf);
+      cuf.print(pkg);
+      cuf.print(path);
+
+    },
 
   }
+
+  // // 除了定义有名称的任务，也可以只定义一个任务
+  // //执行命令：cuf --config ./cuf.config.js
+  // task:function(cuf, pkg, path){
+  //   cuf.log('执行缺省任务');
+  //     cuf.print(cuf);
+  //     cuf.print(pkg);
+  //     cuf.print(path);
+  // }
+
 };
