baaf98d13aac0380537463f91c33cd89dc052bd0
心叶 <yelloxing@gmail.com>
2019-12-12 15:05:59 +0800

    修改命令行模式一些错误

--- bin/options.js
+++ bin/options.js
@@ -2,38 +2,43 @@ module.exports = {
 
   // 命令参数配置
   "config": {
-    help: {
-      short: 'h',
-      info: '[1]Display this help text.',
-      demo: 'cuf --help|-h <term>'
-    },
     force: {
       short: 'f',
       info: '[x]Enforce the current operation.',
       demo: 'cuf --force|-f'
     },
+    version: {
+      short: 'v',
+      info: '[0]Display the version of cuf.',
+      demo: 'cuf --version|-v'
+    },
     config: {
-      info: '[0]Specify a configuration file.',
+      info: '[1]Specify a configuration file.',
       demo: 'cuf --config ./cuf.config.js'
+    },
+    help: {
+      short: 'h',
+      info: '[2]Display this help text.',
+      demo: 'cuf --help|-h <term>'
     },
     delete: {
       short: 'd',
-      info: '[2]delete file or folder.',
+      info: '[3]delete file or folder.',
       demo: 'cuf --delete|-d ./file'
     },
     copy: {
       short: 'c',
-      info: '[3]copy file or folder.',
+      info: '[4]copy file or folder.',
       demo: 'cuf --copy|-c ./sourceFile ./targetFile'
     },
     move: {
       short: 'm',
-      info: '[4]move file or folder.',
+      info: '[5]move file or folder.',
       demo: 'cuf --move|-m ./sourceFile ./targetFile'
     },
-    server:{
+    server: {
       short: 's',
-      info: '[5]render source server.',
+      info: '[6]render source server.',
       demo: 'cuf --server|-s [port|8080]'
     }
   },
@@ -43,11 +48,10 @@ module.exports = {
   Usage: cuf <command>
   
   where <command> is one of:
-    --help, -h, --config, --force, -f, --delete, -d, --move, -m, --copy, -c, --server, -s
+    --help, -h, --config, --force, -f, --delete, -d, --move, -m, --copy, -c, --server, -s, --version, -v
   
   cuf --help|-h <term>       search for help on <term>
   cuf --help|-h              involved overview
     `
 
 };
-
